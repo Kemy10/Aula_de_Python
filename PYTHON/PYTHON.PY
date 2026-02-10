e2ff6be5522030b8bc38b7ddbf25e3fbef8b76b9
Kemy10 <120021911+Kemy10@users.noreply.github.com>
2026-02-09 22:02:29 -0300

    Add files via upload
    Com o professor aprendemos variável, somar e exibir o código.

---
+++ PYTHON/PYTHON.PY
@@ -0,0 +1,26 @@
+print ("Olá Mundo!") 
+
+# se eu quiser comentar várias linhas 
+# ao mesmo tempo é so eu selecionar tudo
+# apertar ctrl + ; ( ele ja deixa comentado as linhas que eu selecionei)
+
+
+#Nrm inteiro
+print (20)
+print (-10)
+
+#Float (Decimal)
+print (10.5)
+print (23.4)
+
+#String
+print ("Olá, estamos na aula aprendendo Python")
+
+#VARIAVEL
+
+idade = 20
+print (idade)
+preco = (39.99)
+print (preco)
+nome  = ("Meu nome é Kemylin")
+print (idade, preco, nome)
---
+++ PYTHON/calculos.py
@@ -0,0 +1,18 @@
+numero1 = 1
+numero2 = 40
+
+somar = numero1 + numero2
+print (somar)
+
+
+#EXERCÍCOS
+#1 - fazer 4 variáveis e atribua um valor inteiro, ao final exiba a soma de todos os valores
+
+Nmr1 = 24
+Nmr2 = 8 
+Nmr3 = 12
+Nmr4 = 56
+
+somar = Nmr1 + Nmr2 + Nmr3 + Nmr4
+print (somar)
+
